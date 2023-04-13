2cc5c9c8f51fe768fc2d0bedad96da64e2b3563b
NERDY-B <isrealakhigbe@gmail.com>
2023-04-13 18:14:28 +0100

    add welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -147,6 +147,10 @@ const controlAddRecipe = async function(newRecipe){
 
 };
 
+const newFeature = function(){
+  console.log('welcome to the application');
+}
+
 const init = function(){
   bookmarksView.addHandlerRender(controlBookmarks)
   recipeView.addHandlerRender(controlRecipes);
@@ -155,7 +159,7 @@ const init = function(){
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe)
-  
+  newFeature();
 };
 
 init();
